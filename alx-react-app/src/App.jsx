67d27dc1a00a14d989b13788858ed137b28a083c
Okwuchi Onyekachi <okwuchiomotolaonyeka@gmail.com>
2025-02-16 18:56:52 +0100

    push

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,17 +1,15 @@
 
 import React from 'react';
-import Header from './components/Header'
-import MainContent from './components/MainContent'
-import Footer from './components/Footer'
+import UserProfile from './components/UserProfile';
+
 
 
 const App = () => {
   return (
     <div>
-      <Header />
-      <MainContent />
-      <Footer />
-    </div>
+    <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
+  </div>
+    
   );
 };
 
