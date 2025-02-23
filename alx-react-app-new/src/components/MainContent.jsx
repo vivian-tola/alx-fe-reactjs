463078058eec8456316b1dad1477d2bc4a829d1b
Okwuchi Onyekachi <okwuchiomotolaonyeka@gmail.com>
2025-02-23 14:57:41 +0100

    add

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -5,6 +5,7 @@ const MainContent = () => {
   return (
         <main style={{ padding: '20px', backgroundColor: '#eaeaea', minHeight: '300px' }}>
           <h2 style={{ color: 'darkgreen', textAlign: 'center' }}>Welcome to My Travel Blog</h2>
+          <h3 style={{ color: 'darkblue', textAlign: 'center'}}> I love to visit New York, Paris, and Tokyo.</h3>
           <p style={{ fontSize: '16px', lineHeight: '1.6', color: '#333' }}>
             Explore the beauty of the world&apos;s most amazing cities with me!
           </p>
