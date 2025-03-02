e52e6eb451ee7f76e40a4a301193138f371cc966
Okwuchi Onyekachi <okwuchiomotolaonyeka@gmail.com>
2025-03-02 10:48:10 +0100

    display inline style

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,13 +1,41 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div style={{ backgroundColor: '#333', padding: '10px', color: 'white' }}>
-      <Link to="/" style={{ color: 'white', margin: '0 10px', textDecoration: 'none' }}>Home</Link>
-      <Link to="/about" style={{ color: 'white', margin: '0 10px', textDecoration: 'none' }}>About</Link>
-      <Link to="/services" style={{ color: 'white', margin: '0 10px', textDecoration: 'none' }}>Services</Link>
-      <Link to="/contact" style={{ color: 'white', margin: '0 10px', textDecoration: 'none' }}>Contact</Link>
+    <div
+      style={{
+        backgroundColor: "#333",
+        padding: "10px",
+        color: "white",
+        display: "flex",
+        justifyContent: "space-between",
+      }}
+    >
+      <Link
+        to="/"
+        style={{ color: "white", margin: "0 10px", textDecoration: "none" }}
+      >
+        Home
+      </Link>
+      <Link
+        to="/about"
+        style={{ color: "white", margin: "0 10px", textDecoration: "none" }}
+      >
+        About
+      </Link>
+      <Link
+        to="/services"
+        style={{ color: "white", margin: "0 10px", textDecoration: "none" }}
+      >
+        Services
+      </Link>
+      <Link
+        to="/contact"
+        style={{ color: "white", margin: "0 10px", textDecoration: "none" }}
+      >
+        Contact
+      </Link>
     </div>
   );
 }
