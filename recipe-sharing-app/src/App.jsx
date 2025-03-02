4648c47dde7643a456af85ef85e87f0a08f43a4e
Okwuchi Onyekachi <okwuchiomotolaonyeka@gmail.com>
2025-03-02 21:58:12 +0100

    zustand

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeList from "./components/RecipeList";
+
+function App() {
+  return (
+    <>
+      <h1>Recipe Manager</h1>
+      <AddRecipeForm />
+      <RecipeList />
+    </>
+  );
+}
+
+export default App;
+
+{
+  /* <div>
+        <h1>Recipe Manager</h1>
+        <AddRecipeForm />
+        <RecipeList />
+      </div> */
+}
