d67bf032cc2e09c26c9d461778dd8d9885fe20e2
Okwuchi Onyekachi <okwuchiomotolaonyeka@gmail.com>
2025-03-09 19:39:49 +0100

    ..add

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -6,12 +6,12 @@ const FormikForm = () => {
   const validationSchema = Yup.object({
     username: Yup.string()
       .required('Username is required'),
-    email: Yup.string()
-      .email('Invalid email address')
-      .required('Email is required'),
-    password: Yup.string()
-      .min(6, 'Password must be at least 6 characters')
-      .required('Password is required'),
+    email: Yup.string().required('Email is required')
+      // .email('Invalid email address')
+      ,
+    password: Yup.string().required('Password is required')
+      // .min(6, 'Password must be at least 6 characters')
+      ,
   });
 
   return (
