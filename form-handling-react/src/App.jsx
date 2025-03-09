1e38ec3b9cdbdba933be0d69360b4537d50f0a63
Okwuchi Onyekachi <okwuchiomotolaonyeka@gmail.com>
2025-03-09 19:25:23 +0100

    addd

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -3,13 +3,16 @@ import './App.css'
 
   import React from 'react';
   import RegistrationForm from './components/RegistrationForm';
-  import FormikForm from './components/formikForm';
+  // eslint-disable-next-line no-unused-vars
+  import formikForm from './components/formikForm';
   
   function App() {
     return (
       <div className="App">
         <h1>User Registration</h1>
         <RegistrationForm />
+        <h2>Form Components</h2>
+        <formikForm /> 
       </div>
     );
   }
