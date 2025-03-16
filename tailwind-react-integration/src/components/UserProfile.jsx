8a39b8d89aeb5164300df869076ba3216ffc26e1
Okwuchi Onyekachi <okwuchiomotolaonyeka@gmail.com>
2025-03-16 23:11:55 +0100

    hover

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,8 +1,8 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 sm:p-4 md:p-8 lg:p-8 sm:max-w-xs md:max-w-sm lg:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
-      <img src="https://via.placeholder.com/150" alt="User" className="rounded-full sm:w-24 sm:h-24  md:w-36 md:h-36 lg:w-36 lg:h-36 mx-auto" />
-      <h1 className="text-lg md:text-xl lg:text-xl text-blue-800 my-4">John Doe</h1>
+    <div className="user-profile bg-gray-100 sm:p-4 md:p-8 lg:p-8 sm:max-w-xs md:max-w-sm lg:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl">
+      <img src="https://via.placeholder.com/150" alt="User" className="rounded-full sm:w-24 sm:h-24  md:w-36 md:h-36 lg:w-36 lg:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out" />
+      <h1 className="text-lg md:text-xl lg:text-xl text-blue-800 my-4 hover:text-blue-500">John Doe</h1>
       <p className="text-gray-600 text-sm md:text-base lg:text-base">
         Developer at Example Co. Loves to write code and explore new
         technologies.
