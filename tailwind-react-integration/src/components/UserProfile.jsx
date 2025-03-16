e08437da41296bdf46b4274c7e8a4798ead55d07
Okwuchi Onyekachi <okwuchiomotolaonyeka@gmail.com>
2025-03-16 23:07:09 +0100

    user..

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 function UserProfile() {
   return (
     <div className="user-profile bg-gray-100 sm:p-4 md:p-8 lg:p-8 sm:max-w-xs md:max-w-sm lg:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
-      <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-24 h-24  md:w-36 md:h-36 lg:w-36 lg:h-36 mx-auto" />
+      <img src="https://via.placeholder.com/150" alt="User" className="rounded-full sm:w-24 sm:h-24  md:w-36 md:h-36 lg:w-36 lg:h-36 mx-auto" />
       <h1 className="text-lg md:text-xl lg:text-xl text-blue-800 my-4">John Doe</h1>
       <p className="text-gray-600 text-sm md:text-base lg:text-base">
         Developer at Example Co. Loves to write code and explore new
