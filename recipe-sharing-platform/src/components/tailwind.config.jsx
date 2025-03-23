984d528243dd0ee1d7b0907751079874d5611038
Okwuchi Onyekachi <okwuchiomotolaonyeka@gmail.com>
2025-03-23 17:22:25 +0100

    recipe

---
+++ recipe-sharing-platform/src/components/tailwind.config.jsx
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    darkMode: false, // You can set it to 'media' or 'class' if needed
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
